5a383d061b97783d76308828f9f76ed29bf8a45d
DmitrijMoreinis <moreinis.dmitrij@gmail.com>
2023-09-02 16:11:59 +0200

    final with feign client and more comments

--- EquipmentTracker/src/main/java/com/activitytracker/EquipmentTracker/repository/RunningShoeRepository.java
+++ EquipmentTracker/src/main/java/com/activitytracker/EquipmentTracker/repository/RunningShoeRepository.java
@@ -2,8 +2,11 @@ package com.activitytracker.EquipmentTracker.repository;
 
 import com.activitytracker.EquipmentTracker.model.RunningShoe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
+@Repository
 
 public interface RunningShoeRepository extends JpaRepository<RunningShoe, Integer> {
 }
--- EquipmentTracker/src/main/java/com/activitytracker/EquipmentTracker/repository/ShoeUserRepository.java
+++ EquipmentTracker/src/main/java/com/activitytracker/EquipmentTracker/repository/ShoeUserRepository.java
@@ -3,9 +3,12 @@ package com.activitytracker.EquipmentTracker.repository;
 import com.activitytracker.EquipmentTracker.model.RunningShoe;
 import com.activitytracker.EquipmentTracker.model.ShoeUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+@Repository
 public interface ShoeUserRepository extends JpaRepository<ShoeUser,Integer> {
 
 
